174d6aeefc423c66401caad2fb113276ce8fcb20
Kazuhisa Ishizaka <iszk1215@gmail.com>
2021-07-07 22:51:02 +0900

    Update

--- test01.cc
+++ test01.cc
@@ -1,3 +1,4 @@
+#include <vector>
 #include <pybind11/pybind11.h>
 
 namespace py = pybind11;
@@ -6,10 +7,11 @@ py::object test() {
     py::object pandas = py::module_::import("pandas");
     py::object read_csv = pandas.attr("read_csv");
     py::object df = read_csv("test01.csv");
-    py::object getitem = df.attr("__getitem__");
-    py::object col_a = getitem("a");
-    py::object mul = col_a.attr("__mul__");
-    py::object ret = mul(2);
+    py::list cols;
+    cols.append("a");
+    cols.append("b");
+    py::object col_a = df.attr("__getitem__")(cols);
+    py::object ret = col_a.attr("__mul__")(2);
 
     return ret;
 }
